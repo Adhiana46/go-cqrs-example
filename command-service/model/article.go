69246d07dfa97ce724d6f71733abf919f04c56b6
Adhiana <adhianamastur48@gmail.com>
2022-12-19 14:44:31 +0700

    fix(command-service): fix created_at, updated_at not send to message broker

--- command-service/model/article.go
+++ command-service/model/article.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type Article struct {
-	ID        int       `db:"id"`
-	Uuid      string    `db:"uuid"`
-	Author    string    `db:"author"`
-	Title     string    `db:"title"`
-	Body      string    `db:"body"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	ID        int       `db:"id" json:"id"`
+	Uuid      string    `db:"uuid" json:"uuid"`
+	Author    string    `db:"author" json:"author"`
+	Title     string    `db:"title" json:"title"`
+	Body      string    `db:"body" json:"body"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
 }
